c67c30e4ae9e0c2466f660276293d6799bca7b44
0xdead6eef <fake@mail.com>
2018-02-15 16:13:00 +0900

    fork from twty

--- twty.go
+++ twty.go
@@ -2,36 +2,38 @@ package main
 
 import (
 	"bufio"
-	"bytes"
-	"compress/gzip"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"html"
 	"io"
 	"io/ioutil"
 	"log"
-	"mime/multipart"
 	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strconv"
 	"strings"
-	"time"
-
+	"syscall"
 	"github.com/fatih/color"
 	"github.com/garyburd/go-oauth/oauth"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
-const (
-	_EmojiRedHeart    = "\u2764"
-	_EmojiHighVoltage = "\u26A1"
+var (
+	PasswordString = os.Getenv("TWTY_PASSWORD") // must be 16, 24 or 32 bytes
+
+	account  = flag.String("a", "", "account")
+	list     = flag.String("l", "", "show list tweets")
+	dotweet  = flag.String("t", "", "tweet message")
+	verbose  = flag.Bool("v", false, "detail display")
+	colorMode = flag.Bool("c", false, "coloring(default off)")
 )
 
-// Account hold information about account
 type Account struct {
 	TimeZone struct {
 		Name       string `json:"name"`
@@ -70,7 +72,6 @@ type Account struct {
 	} `json:"trend_location"`
 }
 
-// Tweet hold information about tweet
 type Tweet struct {
 	Text       string `json:"text"`
 	Identifier string `json:"id_str"`
@@ -102,7 +103,6 @@ type Tweet struct {
 	} `json:"entities"`
 }
 
-// SearchMetadata hold information about search metadata
 type SearchMetadata struct {
 	CompletedIn float64 `json:"completed_in"`
 	MaxID       int64   `json:"max_id"`
@@ -115,7 +115,6 @@ type SearchMetadata struct {
 	SinceIDStr  string  `json:"since_id_str"`
 }
 
-// RSS hold information about RSS
 type RSS struct {
 	Channel struct {
 		Title       string
@@ -130,17 +129,6 @@ type RSS struct {
 			Author      string
 		}
 	}
-}
-
-type files []string
-
-func (f *files) String() string {
-	return strings.Join([]string(*f), ",")
-}
-
-func (f *files) Set(value string) error {
-	*f = append(*f, value)
-	return nil
 }
 
 var oauthClient = oauth.Client{
@@ -164,9 +152,13 @@ func clientAuth(requestToken *oauth.Credentials) (*oauth.Credentials, error) {
 	} else if runtime.GOOS == "plan9" {
 		browser = "plumb"
 	}
-	color.Set(color.FgHiRed)
+	if *colorMode {
+		color.Set(color.FgHiRed)
+	}
 	fmt.Println("Open this URL and enter PIN.")
-	color.Set(color.Reset)
+	if *colorMode {
+		color.Set(color.Reset)
+	}
 	fmt.Println(url)
 	browser, err = exec.LookPath(browser)
 	if err == nil {
@@ -219,49 +211,6 @@ func getAccessToken(config map[string]string) (*oauth.Credentials, bool, error) 
 	return token, authorized, nil
 }
 
-func upload(token *oauth.Credentials, file string, opt map[string]string, res interface{}) error {
-	uri := "https://upload.twitter.com/1.1/media/upload.json"
-	param := make(url.Values)
-	for k, v := range opt {
-		param.Set(k, v)
-	}
-	oauthClient.SignParam(token, http.MethodPost, uri, param)
-	var buf bytes.Buffer
-
-	w := multipart.NewWriter(&buf)
-
-	f, err := os.Open(file)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	fw, err := w.CreateFormFile("media", file)
-	if err != nil {
-		return err
-	}
-	if _, err = io.Copy(fw, f); err != nil {
-		return err
-	}
-	w.Close()
-
-	req, err := http.NewRequest(http.MethodPost, uri, &buf)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", w.FormDataContentType())
-	req.Header.Set("Authorization", "OAuth "+strings.Replace(param.Encode(), "&", ",", -1))
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if res == nil {
-		return nil
-	}
-	return json.NewDecoder(resp.Body).Decode(&res)
-}
-
 func rawCall(token *oauth.Credentials, method string, uri string, opt map[string]string, res interface{}) error {
 	param := make(url.Values)
 	for k, v := range opt {
@@ -270,7 +219,7 @@ func rawCall(token *oauth.Credentials, method string, uri string, opt map[string
 	oauthClient.SignParam(token, method, uri, param)
 	var resp *http.Response
 	var err error
-	if method == http.MethodGet {
+	if method == "GET" {
 		uri = uri + "?" + param.Encode()
 		resp, err = http.Get(uri)
 	} else {
@@ -283,9 +232,6 @@ func rawCall(token *oauth.Credentials, method string, uri string, opt map[string
 	if res == nil {
 		return nil
 	}
-	if debug {
-		return json.NewDecoder(io.TeeReader(resp.Body, os.Stdout)).Decode(&res)
-	}
 	return json.NewDecoder(resp.Body).Decode(&res)
 }
 
@@ -295,50 +241,43 @@ var replacer = strings.NewReplacer(
 	"\t", " ",
 )
 
-const _TimeLayout = "Mon Jan 02 15:04:05 -0700 2006"
-
-func toLocalTime(timeStr string) string {
-	timeValue, err := time.Parse(_TimeLayout, timeStr)
-	if err != nil {
-		return timeStr
-	}
-	return timeValue.Local().Format(_TimeLayout)
-}
-
-func showTweets(tweets []Tweet, asjson bool, verbose bool) {
-	if asjson {
-		for _, tweet := range tweets {
-			json.NewEncoder(os.Stdout).Encode(tweet)
-			os.Stdout.Sync()
-		}
-	} else if verbose {
+func showTweets(tweets []Tweet, verbose bool) {
+	if verbose {
 		for i := len(tweets) - 1; i >= 0; i-- {
 			name := tweets[i].User.Name
 			user := tweets[i].User.ScreenName
 			text := tweets[i].Text
 			text = replacer.Replace(text)
-			color.Set(color.FgHiRed)
+			if *colorMode {
+				color.Set(color.FgHiRed)
+			}
 			fmt.Println(user + ": " + name)
-			color.Set(color.Reset)
-			fmt.Println("  " + html.UnescapeString(text))
+			if *colorMode {
+				color.Set(color.Reset)
+			}
+			fmt.Println("  " + text)
 			fmt.Println("  " + tweets[i].Identifier)
-			fmt.Println("  " + toLocalTime(tweets[i].CreatedAt))
+			fmt.Println("  " + tweets[i].CreatedAt)
 			fmt.Println()
 		}
 	} else {
 		for i := len(tweets) - 1; i >= 0; i-- {
 			user := tweets[i].User.ScreenName
 			text := tweets[i].Text
-			color.Set(color.FgHiRed)
+			if *colorMode {
+				color.Set(color.FgHiRed)
+			}
 			fmt.Print(user)
-			color.Set(color.Reset)
+			if *colorMode {
+				color.Set(color.Reset)
+			}
 			fmt.Print(": ")
-			fmt.Println(html.UnescapeString(text))
+			fmt.Println(text)
 		}
 	}
 }
 
-func getConfig(profile string) (string, map[string]string, error) {
+func getConfig() (string, map[string]string, error) {
 	dir := os.Getenv("HOME")
 	if dir == "" && runtime.GOOS == "windows" {
 		dir = os.Getenv("APPDATA")
@@ -353,21 +292,10 @@ func getConfig(profile string) (string, map[string]string, error) {
 		return "", nil, err
 	}
 	var file string
-	if profile == "" {
+	if *account == "" {
 		file = filepath.Join(dir, "settings.json")
-	} else if profile == "?" {
-		names, err := filepath.Glob(filepath.Join(dir, "settings*.json"))
-		if err != nil {
-			return "", nil, err
-		}
-		for _, name := range names {
-			name = filepath.Base(name)
-			name = strings.TrimLeft(name[8:len(name)-5], "-")
-			fmt.Println(name)
-		}
-		os.Exit(0)
 	} else {
-		file = filepath.Join(dir, "settings-"+profile+".json")
+		file = filepath.Join(dir, "settings-"+*account+".json")
 	}
 	config := map[string]string{}
 
@@ -376,10 +304,19 @@ func getConfig(profile string) (string, map[string]string, error) {
 		return "", nil, err
 	}
 	if err != nil {
-		config["ClientToken"] = "MbartJkKCrSegn45xK9XLw"
-		config["ClientSecret"] = "1nI3dHFtK9UY1kL6UEYWk6r2lFEcNHWhk7MtXe7eo"
+		config["ClientToken"] = os.Getenv("TWTY_CLIENT_TOKEN")
+		config["ClientSecret"] = os.Getenv("TWTY_CLIENT_SECRET")
 	} else {
-		err = json.Unmarshal(b, &config)
+		// decrypt
+		key := []byte(PasswordString)
+		block, err := aes.NewCipher(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		decryptedText := make([]byte, len(b[aes.BlockSize:]))
+		decryptStream := cipher.NewCTR(block, b[:aes.BlockSize])
+		decryptStream.XORKeyStream(decryptedText, b[aes.BlockSize:])
+		err = json.Unmarshal(decryptedText, &config)
 		if err != nil {
 			return "", nil, fmt.Errorf("could not unmarshal %v: %v", file, err)
 		}
@@ -387,142 +324,8 @@ func getConfig(profile string) (string, map[string]string, error) {
 	return file, config, nil
 }
 
-var (
-	debug bool
-)
-
-func readFile(filename string) ([]byte, error) {
-	if filename == "-" {
-		return ioutil.ReadAll(os.Stdin)
-	}
-	return ioutil.ReadFile(filename)
-}
-
-func countToOpt(opt map[string]string, c string) map[string]string {
-	if c != "" {
-		_, err := strconv.Atoi(c)
-		if err == nil {
-			opt["count"] = c
-		}
-	}
-	return opt
-}
-
-func sinceToOpt(opt map[string]string, timeFormat string) map[string]string {
-	return timeFormatToOpt(opt, "since", timeFormat)
-}
-
-func untilToOpt(opt map[string]string, timeFormat string) map[string]string {
-	return timeFormatToOpt(opt, "until", timeFormat)
-}
-
-func timeFormatToOpt(opt map[string]string, key string, timeFormat string) map[string]string {
-	if timeFormat != "" || !isTimeFormat(timeFormat) {
-		return opt
-	}
-	opt[key] = timeFormat
-	return opt
-}
-
-func sinceIDtoOpt(opt map[string]string, id int64) map[string]string {
-	return idToOpt(opt, "since_id", id)
-}
-
-func maxIDtoOpt(opt map[string]string, id int64) map[string]string {
-	return idToOpt(opt, "max_id", id)
-}
-
-func idToOpt(opt map[string]string, key string, id int64) map[string]string {
-	if id < 1 {
-		return opt
-	}
-	opt[key] = strconv.FormatInt(id, 10)
-	return opt
-}
-
-// isTimeFormat returns true if the parameter string matches the format like '[0-9]+-[0-9]+-[0-9]+'
-func isTimeFormat(t string) bool {
-	splitFormat := strings.Split(t, "-")
-	if len(splitFormat) != 3 {
-		return false
-	}
-
-	for _, v := range splitFormat {
-		_, err := strconv.Atoi(v)
-		if err != nil {
-			return false
-		}
-	}
-
-	return true
-}
-
-func main() {
-	var profile string
-	var reply bool
-	var list string
-	var asjson bool
-	var user string
-	var favorite string
-	var search string
-	var stream bool
-	var inreply string
-	var media files
-	var verbose bool
-
-	flag.StringVar(&profile, "a", "", "account")
-	flag.BoolVar(&reply, "r", false, "show replies")
-	flag.StringVar(&list, "l", "", "show tweets")
-	flag.BoolVar(&asjson, "json", false, "show tweets as json")
-	flag.StringVar(&user, "u", "", "show user timeline")
-	flag.StringVar(&favorite, "f", "", "specify favorite ID")
-	flag.StringVar(&search, "s", "", "search word")
-	flag.BoolVar(&stream, "S", false, "stream timeline")
-	flag.StringVar(&inreply, "i", "", "specify in-reply ID, if not specify text, it will be RT.")
-	flag.Var(&media, "m", "upload media")
-	flag.BoolVar(&verbose, "v", false, "detail display")
-	flag.BoolVar(&debug, "debug", false, "debug json")
-
-	var fromfile string
-	var count string
-	var since string
-	var until string
-	var sinceID int64
-	var maxID int64
-
-	flag.StringVar(&fromfile, "ff", "", "post utf-8 string from a file(\"-\" means STDIN)")
-	flag.StringVar(&count, "count", "", "fetch tweets count")
-	flag.StringVar(&since, "since", "", "fetch tweets since date.")
-	flag.StringVar(&until, "until", "", "fetch tweets until date.")
-	flag.Int64Var(&sinceID, "since_id", 0, "fetch tweets that id is greater than since_id.")
-	flag.Int64Var(&maxID, "max_id", 0, "fetch tweets that id is lower than max_id.")
-
-	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, `Usage of twty:
-  -a PROFILE: switch profile to load configuration file.
-  -f ID: specify favorite ID
-  -i ID: specify in-reply ID, if not specify text, it will be RT.
-  -l USER/LIST: show list's timeline (ex: mattn_jp/subtech)
-  -m FILE: upload media
-  -u USER: show user's timeline
-  -s WORD: search timeline
-  -json: as JSON
-  -S: stream timeline
-  -r: show replies
-  -v: detail display
-  -ff FILENAME: post utf-8 string from a file("-" means STDIN)
-  -count NUMBER: show NUMBER tweets at timeline.
-  -since DATE: show tweets created after the DATE (ex. 2017-05-01)
-  -until DATE: show tweets created before the DATE (ex. 2017-05-31)
-  -since_id NUMBER: show tweets that have ids greater than NUMBER.
-  -max_id NUMBER: show tweets that have ids lower than NUMBER.
-`)
-	}
-	flag.Parse()
-
-	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",http2client=0")
-
-	file, config, err := getConfig(profile)
+func loadConfigData() *oauth.Credentials {
+	file, config, err := getConfig()
 	if err != nil {
 		log.Fatal("failed to get configuration:", err)
 	}
@@ -531,171 +334,77 @@ func main() {
 		log.Fatal("faild to get access token:", err)
 	}
 	if authorized {
-		b, err := json.MarshalIndent(config, "", "  ")
+		b, err := json.Marshal(config)
 		if err != nil {
 			log.Fatal("failed to store file:", err)
 		}
-		err = ioutil.WriteFile(file, b, 0700)
+		// create encrypt key
+		key := []byte(PasswordString)
+		block, err := aes.NewCipher(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Create IV
+		cipherText := make([]byte, aes.BlockSize+len(b))
+		iv := cipherText[:aes.BlockSize]
+		if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+			log.Fatal(err)
+		}
+		// encrypt
+		encryptStream := cipher.NewCTR(block, iv)
+		encryptStream.XORKeyStream(cipherText[aes.BlockSize:], b)
+		err = ioutil.WriteFile(file, cipherText, 0700)
 		if err != nil {
 			log.Fatal("failed to store file:", err)
 		}
 	}
+	return token
+}
 
-	if len(media) > 0 {
-		res := struct {
-			MediaID          int64  `json:"media_id"`
-			MediaIDString    string `json:"media_id_string"`
-			Size             int    `json:"size"`
-			ExpiresAfterSecs int    `json:"expires_after_secs"`
-			Image            struct {
-				ImageType string `json:"image_type"`
-				W         int    `json:"w"`
-				H         int    `json:"h"`
-			} `json:"image"`
-		}{}
-		for i := range media {
-			err = upload(token, media[i], nil, &res)
-			if err != nil {
-				log.Fatal("failed to upload media:", err)
-			}
-			media[i] = res.MediaIDString
-		}
+func main() {
+	flag.Parse()
+	fmt.Fprintf(os.Stderr, "password?\n")
+
+	password, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+			log.Fatal(err)
 	}
+	if l := len(password); l != 16 && l != 24 && l != 32 {
+		log.Fatal("invalid password length. it must be 16, 24 or 32 bytes")
+	}
+	PasswordString = string(password)
 
-	if len(search) > 0 {
-		res := struct {
-			Statuses       []Tweet `json:"statuses"`
-			SearchMetadata `json:"search_metadata"`
-		}{}
-		opt := map[string]string{"q": search}
-		opt = countToOpt(map[string]string{"q": search}, count)
-		opt = sinceToOpt(opt, since)
-		opt = untilToOpt(opt, until)
-		err := rawCall(token, http.MethodGet, "https://api.twitter.com/1.1/search/tweets.json", opt, &res)
-		if err != nil {
-			log.Fatal("failed to get statuses:", err)
-		}
-		showTweets(res.Statuses, asjson, verbose)
-	} else if reply {
-		var tweets []Tweet
-		err := rawCall(token, http.MethodGet, "https://api.twitter.com/1.1/statuses/mentions_timeline.json", countToOpt(map[string]string{}, count), &tweets)
-		if err != nil {
-			log.Fatal("failed to get tweets:", err)
-		}
-		showTweets(tweets, asjson, verbose)
-	} else if list != "" {
-		part := strings.SplitN(list, "/", 2)
+	token := loadConfigData()
+
+	if len(*list) > 0 {
+		part := strings.SplitN(*list, "/", 2)
 		if len(part) == 1 {
 			var account Account
-			err := rawCall(token, http.MethodGet, "https://api.twitter.com/1.1/account/settings.json", nil, &account)
+			err := rawCall(token, "GET", "https://api.twitter.com/1.1/account/settings.json", nil, &account)
 			if err != nil {
 				log.Fatal("failed to get account:", err)
 			}
 			part = []string{account.ScreenName, part[0]}
 		}
 		var tweets []Tweet
-		opt := map[string]string{"owner_screen_name": part[0], "slug": part[1]}
-		opt = countToOpt(opt, count)
-		opt = sinceIDtoOpt(opt, sinceID)
-		opt = maxIDtoOpt(opt, maxID)
-		err := rawCall(token, http.MethodGet, "https://api.twitter.com/1.1/lists/statuses.json", opt, &tweets)
+		err := rawCall(token, "GET", "https://api.twitter.com/1.1/lists/statuses.json", map[string]string{"owner_screen_name": part[0], "slug": part[1]}, &tweets)
 		if err != nil {
 			log.Fatal("failed to get tweets:", err)
 		}
-		showTweets(tweets, asjson, verbose)
-	} else if user != "" {
+		showTweets(tweets, *verbose)
+	} else if *dotweet != "" {
+		var tweet Tweet
+		err := rawCall(token, "POST", "https://api.twitter.com/1.1/statuses/update.json", map[string]string{"status": *dotweet, "in_reply_to_status_id": ""}, &tweet)
+		if err != nil {
+			log.Fatal("failed to post tweet:", err)
+		}
+		fmt.Println("tweeted:", tweet.Identifier)
+	} else if flag.NArg() == 0 {
 		var tweets []Tweet
-		opt := map[string]string{"screen_name": user}
-		opt = countToOpt(opt, count)
-		opt = sinceIDtoOpt(opt, sinceID)
-		opt = maxIDtoOpt(opt, maxID)
-		err := rawCall(token, http.MethodGet, "https://api.twitter.com/1.1/statuses/user_timeline.json", opt, &tweets)
+		err := rawCall(token, "GET", "https://api.twitter.com/1.1/statuses/home_timeline.json", map[string]string{}, &tweets)
 		if err != nil {
 			log.Fatal("failed to get tweets:", err)
 		}
-		showTweets(tweets, asjson, verbose)
-	} else if favorite != "" {
-		err := rawCall(token, http.MethodPost, "https://api.twitter.com/1.1/favorites/create.json", map[string]string{"id": favorite}, nil)
-		if err != nil {
-			log.Fatal("failed to create favorite:", err)
-		}
-		color.Set(color.FgHiRed)
-		fmt.Print(_EmojiRedHeart)
-		color.Set(color.Reset)
-		fmt.Println("favorited")
-	} else if stream {
-		uri := "https://userstream.twitter.com/1.1/user.json"
-		param := make(url.Values)
-		oauthClient.SignParam(token, http.MethodGet, uri, param)
-		uri = uri + "?" + param.Encode()
-		resp, err := http.Get(uri)
-		if err != nil {
-			log.Fatal("failed to get tweets:", err)
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode != 200 {
-			log.Fatal("failed to get tweets:", err)
-		}
-		var buf *bufio.Reader
-		if resp.Header.Get("Content-Encoding") == "gzip" {
-			gr, err := gzip.NewReader(resp.Body)
-			if err != nil {
-				log.Fatal("failed to make gzip decoder:", err)
-			}
-			buf = bufio.NewReader(gr)
-		} else {
-			buf = bufio.NewReader(resp.Body)
-		}
-		var last []byte
-		for {
-			b, _, err := buf.ReadLine()
-			last = append(last, b...)
-			var tweets [1]Tweet
-			err = json.Unmarshal(last, &tweets[0])
-			if err != nil {
-				continue
-			}
-			last = []byte{}
-			if tweets[0].Identifier != "" {
-				showTweets(tweets[:], asjson, verbose)
-			}
-		}
-	} else if fromfile != "" {
-		text, err := readFile(fromfile)
-		if err != nil {
-			log.Fatal("failed to read a new tweet:", err)
-		}
-		var tweet Tweet
-		err = rawCall(token, http.MethodPost, "https://api.twitter.com/1.1/statuses/update.json", map[string]string{"status": string(text), "in_reply_to_status_id": inreply, "media_ids": media.String()}, &tweet)
-		if err != nil {
-			log.Fatal("failed to post tweet:", err)
-		}
-		fmt.Println("tweeted:", tweet.Identifier)
-	} else if flag.NArg() == 0 && len(media) == 0 {
-		if inreply != "" {
-			var tweet Tweet
-			err := rawCall(token, http.MethodPost, "https://api.twitter.com/1.1/statuses/retweet/"+inreply+".json", countToOpt(map[string]string{}, count), &tweet)
-			if err != nil {
-				log.Fatal("failed to retweet:", err)
-			}
-			color.Set(color.FgHiYellow)
-			fmt.Print(_EmojiHighVoltage)
-			color.Set(color.Reset)
-			fmt.Println("retweeted:", tweet.Identifier)
-		} else {
-			var tweets []Tweet
-			err := rawCall(token, http.MethodGet, "https://api.twitter.com/1.1/statuses/home_timeline.json", countToOpt(map[string]string{}, count), &tweets)
-			if err != nil {
-				log.Fatal("failed to get tweets:", err)
-			}
-			showTweets(tweets, asjson, verbose)
-		}
-	} else {
-		var tweet Tweet
-		err = rawCall(token, http.MethodPost, "https://api.twitter.com/1.1/statuses/update.json", map[string]string{"status": strings.Join(flag.Args(), " "), "in_reply_to_status_id": inreply, "media_ids": media.String()}, &tweet)
-		if err != nil {
-			log.Fatal("failed to post tweet:", err)
-		}
-		fmt.Println("tweeted:", tweet.Identifier)
+		showTweets(tweets, *verbose)
 	}
 }
